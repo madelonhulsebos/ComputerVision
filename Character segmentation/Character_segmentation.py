61abc7f0708e2ed1ec63740e636021e42feb1ae8
Madelon Hulsebos <m.hulsebos-1@student.tudelft.nl>
2017-06-21 13:29:36 +0200

    Added dilation and erosion to character segementation

--- Character segmentation/Character_segmentation.py
+++ Character segmentation/Character_segmentation.py
@@ -22,7 +22,7 @@ for filename in positive_images[20:40] :
 
     img_original = cv2.imread('../License plate detection/HOG/matches/' + filename)
     img_gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
-    img_gauss_filtered = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    img_gauss_filtered = cv2.GaussianBlur(img_gray, (7, 7), 0)
     _, img_binary = cv2.threshold(img_gauss_filtered, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     (img_contours, contours,_) = cv2.findContours(img_binary.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -69,6 +69,8 @@ for filename in positive_images[20:40] :
     img_gray_lp = cv2.cvtColor(img_lp, cv2.COLOR_BGR2GRAY)
     img_gauss_filtered_lp = cv2.GaussianBlur(img_gray_lp, (1, 1), 0)
     ret, img_binary_lp = cv2.threshold(img_gray_lp, 100, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img_binary_lp = cv2.erode(img_binary_lp, (5, 5))
+    img_binary_lp = cv2.dilate(img_binary_lp, (3, 3))
 
     LP_WIDTH = img_binary_lp.shape[0]
     LP_HEIGHT = img_binary_lp.shape[1]
@@ -88,7 +90,7 @@ for filename in positive_images[20:40] :
     for contour in contours:
 
         intX, intY, intWidth, intHeight = cv2.boundingRect(contour)
-        if intWidth < LP_WIDTH/3 and intHeight > 1*LP_HEIGHT/5 : #LP_WIDTH/20 and intWidth < 4*LP_WIDTH/7 and intHeight > 2*LP_HEIGHT/10 and intHeight < 9*LP_HEIGHT/10:
+        if intWidth < LP_WIDTH/2 and intHeight > 1*LP_HEIGHT/6 : #LP_WIDTH/20 and intWidth < 4*LP_WIDTH/7 and intHeight > 2*LP_HEIGHT/10 and intHeight < 9*LP_HEIGHT/10:
             char_contours.append(contour)
             cv2.rectangle(img_lp, (intX, intY), (intX + intWidth, intY + intHeight), (0, 255, 0), 1)
 
