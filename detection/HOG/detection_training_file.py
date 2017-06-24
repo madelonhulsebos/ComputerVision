d61586184f627b0298614825d3dec930d67f8a62
Madelon Hulsebos <m.hulsebos-1@student.tudelft.nl>
2017-06-24 14:38:00 +0200

    Restructured project and integrated detection and segmentation components

---
+++ detection/HOG/detection_training_file.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from os import walk
+from sklearn.externals import joblib
+from skimage.color import rgb2gray
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+
+# Extract HOG features from images
+def extract_hog(img):
+    return hog(rgb2gray(resize(img,
+                               (40, 80),
+                               mode='constant')),
+               orientations=16,
+               pixels_per_cell=(8, 8),
+               cells_per_block=(4, 4),
+               block_norm='L2')
+
+# Extract images with and without license plate
+positive_dir = '../../datasets/acme_licenses'
+negative_dir = '../../datasets/negative_instances'
+
+_, _, positive_images = next(walk(positive_dir))
+_, _, negative_images = next(walk(negative_dir))
+
+# Construct train dataset and labels
+X = np.concatenate((
+    np.array([extract_hog(imread('%s/%s' % (positive_dir, filename))) for filename in positive_images]),
+    np.array([extract_hog(imread('%s/%s' % (negative_dir, filename))) for filename in negative_images])
+))
+Y = np.concatenate((np.ones(len(positive_images)), np.zeros(len(negative_images))))
+
+# Apply PCA
+pca = PCA(n_components=.99)
+X = pca.fit_transform(X)
+
+# Train and fit classifier
+classifier = SVC()
+classifier.fit(X, Y)
+
+# Save trained models
+clsf_filename = 'detection/HOG/detection_classifier'
+joblib.dump(classifier, clsf_filename)
+
+pca_filename = 'detection/HOG/detection_pca'
+joblib.dump(pca, pca_filename)
